532f8c9005d91b6553057c7d28fb2a6f812453cc
Simon Pastor <68235075+simonjpastor@users.noreply.github.com>
2021-07-27 10:57:07 -0400

    Add location to searches

--- app.py
+++ app.py
@@ -20,14 +20,37 @@ def find_job_booz(search):
     response = requests.get(search, headers={'User-Agent': 'Chrome/86.0.4240.111'})
     soup = BeautifulSoup(response.text, "html.parser")
 
-    jobs = soup.find_all(class_="cell-title")
-    st.write()
+    jobs_and_locations = soup.find_all("td")
+    positions = soup.find_all(class_="cell-title")
+    jobs = []
+    links = []
+    locations = []
+    excluding_list = []
+
     st.markdown("**Booz Allen**")
-    for i in jobs:
-        job = i.find("a").text
-        link = i.find("a")["href"]
-        st.markdown(f"[{job}]({link})")
-    st.write()
+
+    for i in positions:
+        excluding_list.append(i)
+        jobs.append(i.find('a').text)
+        links.append(i.find('a')["href"])
+    for j in jobs_and_locations:
+        if j not in excluding_list:
+            locations.append(j.text)
+    for f in range (0,len(locations)):
+        st.markdown(f"[{jobs[f]}]({links[f]}) - {locations[f]}")
+
+    #jobs = soup.find_all("td")
+    #st.write(soup)
+    #count = 2
+    #for i in jobs:
+        #if count % 2 ==0:
+            #job = i.find("a").text
+            #link = i.find("a")["href"]
+            #count += 1
+        #elif count %2 != 0:
+            #location = i.text
+            #st.markdown(f"[{job}]({link}) - {location}")
+    #st.write()
 
 def find_job_bcg(search):
     search = f'https://careers.bcg.com/search-results?m=3&keywords={search}'
@@ -35,7 +58,7 @@ def find_job_bcg(search):
     soup = BeautifulSoup(response.text, "html.parser")
 
     jobs = soup.select("div")
-    print("BCG")
+    #print("BCG")
     #for i in jobs:
         #print(i.find(class_="job-title").text)
 
