f1dd24bec20941d03cd393b0b9586470fbf2cfc4
Simon Pastor <68235075+simonjpastor@users.noreply.github.com>
2021-07-27 17:28:41 -0400

    Change approach from Xpath to CSS selector

--- app.py
+++ app.py
@@ -60,6 +60,7 @@ def find_job_bcg(search):
     driver.implicitly_wait(10)
     driver.find_elements_by_xpath("/html/body/div[1]/section/div/div/header/div/section/div/div/div/div[2]/button")[0].click()
     html = driver.page_source
+    driver.quit()
     soup = BeautifulSoup(html, "html.parser")
 
     jobs = soup.select("div .information")
@@ -74,8 +75,8 @@ def find_job_mckinsey(search):
     #driver.get("chrome://settings/clearBrowserData")
     driver.get(f'https://www.mckinsey.com/careers/search-jobs#?query={search}')
     driver.implicitly_wait(8)
-    st.write(driver.find_elements_by_xpath("/html/body/div[1]/div[2]/div/div/div[2]/a[1]"))
-    driver.find_elements_by_xpath("/html/body/div[1]/div[2]/div/div/div[2]/a[1]")[0].click()
+    driver.find_elements_by_css_selector("body > div.cookie-warning-wrapper.-show > div.box > div > div > div.cookie-compliance.row > a.btn.btn-fill.cookie-btn.accept-btn.at-element-click-tracking")[0].click()
+    #driver.find_elements_by_xpath("/html/body/div[1]/div[2]/div/div/div[2]/a[1]")[0].click()
     html = driver.page_source
     driver.quit()
     soup = BeautifulSoup(html, "html.parser")
