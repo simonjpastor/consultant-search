69213a332f5a3e880536c6f0434ffdf0a3c77ae1
Simon Pastor <68235075+simonjpastor@users.noreply.github.com>
2021-10-05 19:33:19 -0400

    Updating app and test

--- app.py
+++ app.py
@@ -1,122 +1,131 @@
-import pandas as pd
-from bs4 import BeautifulSoup
-import requests
-from selenium import webdriver
-import chromedriver_binary
-import streamlit as st
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
+from tweepy import OAuthHandler
+from tweepy import API, error
+from tweepy import Cursor
+from datetime import datetime, date, time, timedelta
+from collections import Counter
+import sys
 import time
+import json
+from datetime import datetime
+import pandas as pd
+import random
+from config.py import CONSUMER_KEY,CONSUMER_SECRET,ACCESS_TOKEN_KEY, ACCESS_TOKEN_SCRET
 
-APP_NAME = "Consulting Job Search"
+APP_NAME = "TwittLists"
 
 st.title(APP_NAME)
 
-search = st.text_input("Enter your search term", "Junior Analyst")
+search = st.text_input("Enter the accounts", "@GretaThunberg, @UNEP")
 
 submit_button = st.button('Submit', key="search_submit")
 
 
-jobs = []
-links = []
+consumer_key=CONSUMER_KEY
+consumer_secret=CONSUMER_SECRET
+access_token_key=ACCESS_TOKEN_KEY
+access_token_secret=ACCESS_TOKEN_SCRET
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token_key, access_token_secret)
+api = API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
 
-def find_job_mckinsey(search):
-    options = Options()
-    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.517 Safari/537.36'
-    options.add_argument('user-agent={0}'.format(user_agent))
+civic_lists = []
+civic_lists_history = []
+civic_name = []
+civic_dates = []
+civic_followers = []
+civic_members = []
+all_members = []
 
-    driver = webdriver.Chrome(options=options)
-    wait = WebDriverWait(driver, 10)
-    chrome_options = Options()
-    chrome_options.add_argument("--disable-extensions")
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.164 Safari/537.36"')
-    #driver.delete_all_cookies()
-    #driver.get("chrome://settings/clearBrowserData")
-    driver.get(f'https://www.mckinsey.com/careers/search-jobs#?query={search}')
-    driver.implicitly_wait(9)
-    driver.find_elements_by_xpath("/html")[0].click()
-    #driver.find_elements_by_css_selector("body > div.cookie-warning-wrapper.-show > div.box > div > div > div.cookie-compliance.row > a.btn.btn-fill.cookie-btn.accept-btn.at-element-click-tracking")[0].click()
-    #st.write(driver.find_elements_by_xpath("/html/body/div[1]/div[2]/div/div/div[2]"))
-    #driver.find_elements_by_xpath("/html/body/div[1]/div[2]/div/div/div[2]/a[1]")[0].click()
-    html = driver.page_source
-    #st.write(html)
-    soup = BeautifulSoup(html, "html.parser")
-    jobs = soup.select(".job-listing-link")
+new_civic_lists = []
+dict_with_people_list = []
 
-    st.markdown("**McKinsey & Company**")
-    for i in jobs:
-        st.markdown(f'[{i["data-layer-text"]}](https://www.mckinsey.com/careers{i["href"][1:]}) - {i.find(class_="city ng-binding ng-scope").text}')
+cool_people = {}
+cool_followers = {}
 
-    return driver
 
-def find_job_booz(search):
-    search = f'https://careers.boozallen.com/jobs/search/{search}'
-    response = requests.get(search, headers={'User-Agent': 'Chrome/86.0.4240.111'})
-    soup = BeautifulSoup(response.text, "html.parser")
+def initial_function(dict_with_people):
+    #Use this function at the very beginning and after each successful final_members() function
+    for peoples in dict_with_people.keys():
+        dict_with_people_list.append(peoples)
+    return dict_with_people_list
 
-    jobs_and_locations = soup.find_all("td")
-    positions = soup.find_all(class_="cell-title")
-    jobs = []
-    links = []
-    locations = []
-    excluding_list = []
+def members_to_lists(dict_with_people,looking_for):
+    for people in dict_with_people_list:
+        try :
+            for i in api.lists_memberships(people, count=10):
+                if i.id not in civic_lists and i.id not in civic_lists_history:
+                    for j in looking_for:
+                        if j in i.name.lower():
+                            civic_lists.append(i.id)
+                            civic_lists_history.append(i.id)
+                            civic_name.append(i.name)
+                            civic_dates.append(i.created_at)
+                            civic_followers.append(i.subscriber_count)
+                            civic_members.append(i.member_count)
+                            if len(dict_with_people_list) % 10 == 0:
+                                print(len(dict_with_people_list))
+        except error.TweepError:
+            pass
+        dict_with_people_list.remove(people)
+    return civic_lists
 
-    st.markdown("**Booz Allen**")
 
-    for i in positions:
-        excluding_list.append(i)
-        jobs.append(i.find('a').text)
-        links.append(i.find('a')["href"])
-    for j in jobs_and_locations:
-        if j not in excluding_list:
-            locations.append(j.text)
-    for f in range (0,len(locations)):
-        st.markdown(f'[{jobs[f]}]({links[f]}) - {locations[f]}')
+def list_to_members(dict_with_people,looking_for):
+    for i in members_to_lists(dict_with_people,looking_for):
+        try :
+            all_members.append(api.list_members(list_id=i))
+        except error.TweepError:
+            pass
+        civic_lists.remove(i)
+    return all_members
 
-def find_job_bcg(search, driver):
-    driver.get(f"https://careers.bcg.com/search-results?m=3&keywords={search}")
-    driver.implicitly_wait(10)
-    driver.find_elements_by_xpath("/html/body/div[1]/section/div/div/header/div/section/div/div/div/div[2]/button")[0].click()
-    html = driver.page_source
-    #driver.quit()
-    soup = BeautifulSoup(html, "html.parser")
+def final_members(dict_with_people,looking_for):
+    for members in list_to_members(dict_with_people,looking_for):
+        for i in members:
+            if i.screen_name in cool_people.keys():
+                cool_people[i.screen_name] += 1
+            else:
+                cool_people[i.screen_name] = 0
+    return cool_people
 
-    jobs = soup.select("div .information")
-    job_link = {}
-    st.markdown("**BCG**")
-    for i in jobs:
-        st.markdown(f'[{i.find("a")["data-ph-at-job-title-text"]}]({i.find("a")["href"]})')
-
-    return driver
-
-def find_job_mercer(search, driver):
-    driver.get(f"https://careers.mmc.com/global/en/search-results?keywords={search}")
-    driver.implicitly_wait(9)
-    #driver.find_elements_by_xpath("/html/body/div[1]/section/div/div/header/div/section/div/div/div/div[2]/button/ppc-content")[0].click()
-    driver.find_elements_by_xpath("/html")[0].click()
-    html = driver.page_source
-    soup = BeautifulSoup(html, "html.parser")
-    st.markdown("**Mercer**")
-    for i in soup.find_all(class_="information"):
-        if i.find("span").find("a"):
-                x = i.find("span").find("a")
-                st.markdown(f'[{x["data-ph-at-job-title-text"]}]({x["href"]}) - {x["data-ph-at-job-location-area-text"]}')
+def top5(dict_with_people,looking_for):
+    ranking = []
+    number = []
+    for i,j in final_members(dict_with_people,looking_for).items():
+        if len(ranking) < 300:
+            ranking.append(i)
+            number.append(j)
         else:
-            continue
-    return driver
+            if j > min(number):
+                ranking.pop(number.index(min(number)))
+                number.pop(number.index(min(number)))
+                ranking.append(i)
+                number.append(j)
+            else:
+                continue
 
-if submit_button:
-    search = search.replace(" ","%20")
-    driver = find_job_mckinsey(search)
-    st.write()
-    driver = find_job_bcg(search, driver)
-    st.write()
-    find_job_booz(search)
-    st.write()
-    driver = find_job_mercer(search,driver)
-    driver.quit()
+    x = list(zip(ranking,number))
+    return x
+
+def run(iterations):
+    initial_function(cool_people)
+    top5(cool_people,looking_for_list)
+    print("_____")
+    while iterations > 1:
+        print(f"{iterations} iteration successful")
+        iterations = iterations - 1
+        initial_function(cool_people)
+        top5(cool_people,looking_for_list)
+    final_results = end(cool_people)
+    return final_results
+
+def end(results):
+    results = sorted(results.items(), key=lambda x: x[1], reverse=True)
+    values = []
+    accounts = []
+    for i in results:
+        accounts.append(i[0])
+        values.append(i[1])
+    final_results = pd.DataFrame.from_dict({"Accounts":accounts,"Counts":values})
+    return final_results
 
