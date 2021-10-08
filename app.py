ed4866473bbc19ebc013f4ac5a59199cfa55c3ce
Simon Pastor <68235075+simonjpastor@users.noreply.github.com>
2021-10-07 20:03:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ import random
 #import config
 import streamlit as st
 from streamlit_tags import st_tags
-import streamlit_analytics
+#import streamlit_analytics
 #from config.py import CONSUMER_KEY,CONSUMER_SECRET,ACCESS_TOKEN_KEY, ACCESS_TOKEN_SCRET
 
-streamlit_analytics.start_tracking()
-streamlit_analytics.track(save_to_json="/Users/simonpastor/Documents/Github/twittlist/file.json")
+#streamlit_analytics.start_tracking()
+#streamlit_analytics.track(save_to_json="/Users/simonpastor/Documents/Github/twittlist/file.json")
 
 APP_NAME = "TwittLists"
 
