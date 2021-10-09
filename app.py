26bc778325896e80116ebc008f35f2e4c50d7b97
Simon Pastor <68235075+simonjpastor@users.noreply.github.com>
2021-10-09 11:44:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,8 @@ text = st_tags(
 
 submit_button = st.button('Submit', key="search_submit")
 
-random_number = random.randrange(1, 3)
+#random_number = random.randrange(1, 3)
+random_number = 3
 consumer_key=st.secrets[f"CONSUMER_KEY{random_number}"]
 consumer_secret=st.secrets[f"CONSUMER_SECRET{random_number}"]
 access_token_key=st.secrets[f"ACCESS_TOKEN_KEY{random_number}"]
