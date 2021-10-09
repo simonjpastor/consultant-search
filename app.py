23c4c2cc25d49e517168ac1c9458287c2f309b57
Simon Pastor <68235075+simonjpastor@users.noreply.github.com>
2021-10-09 18:19:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -187,7 +187,7 @@ if submit_button:
             st.write(x.screen_name)
             if len(x.description) > 3:
                 st.write(x.description)
-            st.write(f"Description: {x.status.text}")
+            st.write(f"Last Tweet: {x.status.text}")
             st.write(f"Followers Count: {x.followers_count}")
             st.write(f"Following: {x.friends_count}")
     else:
@@ -200,7 +200,7 @@ if submit_button:
             st.write(x.screen_name)
             if len(x.description) > 3:
                 st.write(x.description)
-            st.write(f"Description: {x.status.text}")
+            st.write(f"Last Tweet: {x.status.text}")
             st.write(f"Followers Count: {x.followers_count}")
             st.write(f"Following: {x.friends_count}")
         #st.markdown(f"""<h3 style='text-align: center'><img src={x.profile_image_url}></img>{x.name}<br>{x.screen_name}<br>{x.description}<br>Followers Count: {x.followers_count}<br>Subscribers: {x.friends_count}</h3>""",unsafe_allow_html=True)
