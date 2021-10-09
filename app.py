cfd3ab1fb87505897a45de1b1270db7c7b3d7759
Simon Pastor <68235075+simonjpastor@users.noreply.github.com>
2021-10-09 12:12:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -182,8 +182,8 @@ if submit_button:
         st.image(x.profile_image_url, width=120, height=67)
         st.write(x.screen_name)
         st.write(x.description)
-        st.write("Followers Count: "x.followers_count)
-        st.write("Following: "x.friends_count)
+        st.write(f"Followers Count: {x.followers_count}")
+        st.write(f"Following: {x.friends_count}")
         #st.markdown(f"""<h3 style='text-align: center'><img src={x.profile_image_url}></img>{x.name}<br>{x.screen_name}<br>{x.description}<br>Followers Count: {x.followers_count}<br>Subscribers: {x.friends_count}</h3>""",unsafe_allow_html=True)
 
 
