882181a1daa19e45a699aa7293e957e3b31c31cc
Simon Pastor <68235075+simonjpastor@users.noreply.github.com>
2021-10-21 17:06:27 -0400

    Add json

--- app.py
+++ app.py
@@ -187,18 +187,7 @@ def google_sheets():
     sheet = client.open_by_key(spreadsheet_key)
 
     # add credentials to the account
-    creds = ServiceAccountCredentials.from_json_keyfile_name({
-    "type": st.secrets["type"],
-    "project_id": st.secrets["project_id"],
-    "private_key_id": st.secrets["private_key_id"],
-    "private_key": st.secrets["private_key"],
-    "client_email": st.secrets["client_email"],
-    "client_id": st.secrets["client_id"],
-    "auth_uri": st.secrets["auth_uri"],
-    "token_uri": st.secrets["token_uri"],
-    "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
-    "client_x509_cert_url": st.secrets["client_x509_cert_url"]
-    }, scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name('x3.json', scope)
 
     # authorize the clientsheet
     client = gspread.authorize(creds)
