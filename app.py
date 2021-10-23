4383a87f6036bb861a2e2c34f68c58db3c68df8c
Simon Pastor <68235075+simonjpastor@users.noreply.github.com>
2021-10-23 19:15:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ st.markdown(f"""<head><!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id={st.secrets['TRACKING_ID']}"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
+  function gtag(){{dataLayer.push(arguments);}}
   gtag('js', new Date());
 
   gtag('config', {st.secrets['TRACKING_ID']});
