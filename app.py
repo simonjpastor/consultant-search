4d0e5bee70f025a3adb2eb31939a0082575dd13c
Simon Pastor <68235075+simonjpastor@users.noreply.github.com>
2023-01-29 17:20:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -259,7 +259,6 @@ if submit_button:
     for i in search_values[random_topic]:
         st.write(i)
         for list_name in api.lists_memberships(screen_name=i, count=10):
-            st.write(list_name.full_name)
             suggested_texts.append(list_name.full_name)
         #st.write(suggested_texts[0])
     suggestions = most_common(suggested_texts)[:4]
